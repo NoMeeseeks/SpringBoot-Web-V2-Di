bf471e8887912656bae5c0b148fdb0bcc634602c
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-06-11 08:25:11 -0500

    Inicio del proyecto

---
+++ src/main/java/org/example/springbootwebdi/SpringbootwebdiApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootwebdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebdiApplication.class, args);
+    }
+
+}
